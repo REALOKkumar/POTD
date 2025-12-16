c57273544cc2cfae2b8e4d424a76a87b8f15629b
Alok Kumar <alokgysh@gmail.com>
2025-12-17 01:42:34 +0530

    17 Dec

---
+++ all_ball_to_each_box.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        
+        vector<int> answer;
+        for (int i=0;i<boxes.size();i++){
+            int operations=0;
+            for(int j=0;j<boxes.size();j++){
+                if(boxes[j]=='0') continue;
+                
+                else {
+                    
+                    operations+=abs(i - j);
+                }
+
+            }
+            answer.push_back(operations);
+        }
+
+
+        return answer;
+        
+    }
+};
+
+int main(){
+    Solution sol;
+    string s="001011";
+
+    vector<int> ans =sol.minOperations(s);
+    for(int i:ans){
+        cout<<i<<" ";
+    }
+    return 0;
+}
