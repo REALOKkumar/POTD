b2d3b3c784145ca1a8672db6128c80798b923252
Alok Kumar <alokgysh@gmail.com>
2026-01-10 10:35:59 +0530

    10 Jan

---
+++ questions_other_than_POTD/equivilant_string.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1="",s2="";
+        for(string i:word1){
+            s1+=i;
+        }
+
+        for(string i:word2){
+            s2+=i;
+        }
+        if(s1==s2) return true;
+
+        return false;
+        
+    }
+};
+
+
+
+
+int main(){
+    Solution sol;
+    vector<string> word1 = {"ab", "c"}, word2 = {"a", "bc"};
+    cout<<sol.arrayStringsAreEqual(word1,word2);
+    return 0;
+
+
+
+
+}
---
+++ questions_other_than_POTD/test.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    vector<vector<int>>  max_data;
+    vector<int> row_max,col_max;
+    int a=max(4,5);
+    cout<<a;
+    
+    
+
+}
